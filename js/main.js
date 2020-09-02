f848bcb00a3b9c8416fd0f33f0280d5094cc4dc8
AllanLimitless <Allanblackett@gmail.com>
2020-09-02 13:55:49 -0400

    working on sliders

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-let rangeslider = document.getElementById("sliderRange");
-let output = document.getElementById("demo");
-output.innerHTML = rangeslider.value;
+let rangeslider = document.getElementsByClassName("rangeslider");
+// let output = document.getElementById("demo");
+// // let output2 = document.getElementById("demo2");
 
-rangeslider.oninput = function() {
-  output.innerHTML = this.value;
+// output.innerHTML = rangeslider.value;
+// output2.innerHTML = rangeslider.value;
+
+
+
+
+// rangeslider.oninput = function() {
+//   output.innerHTML = this.value;
+//   document.getElementById("demo2").innerHTML = this.value;
+// }
+
+for(let i=0; i<(rangesliders.length); i++) {
+  rangesliders[i].addEventListener('input', function() {
+    document.getElementById(this.getAttribute('id')+'score').innerText = this.value;
+  });
 }
